6edfdf401cb4bf925dd5f21a29a67428e22941ac
Shaurya(Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-08-01 21:18:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '6744296768:AAGZxSb2XX_qOUbgzV31siivLMckDKYEpbs' # Get Your Telegram Bot Token from @BotFather
+TOKEN = '' # Get Your Telegram Bot Token from @BotFather
 OWNER_ID = '1737646273' # Add Here your telegram id 
